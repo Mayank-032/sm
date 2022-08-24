9fbb3bb9469cdc8817257a7ca19a676e04b3d94b
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-08-24 23:46:45 +0530

    router added

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var mongoose = require("mongoose");
+var signinRoute = require("./controllers/signin.js");
+var signupRoute = require("./controllers/signup.js");
 
 var app = express();
-app.get("/", function(req, res) {
-    res.send("hello")
-})
+// app.use("/signin", signinRoute);
+app.use("/signup", signupRoute);
 
-app.listen(3000, function() {
-    console.log("Working on 3000!!!")
-})
+app.listen(3000, function () {
+  console.log("Working on 3000!!!");
+});
