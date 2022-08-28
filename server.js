7c504d72e21e897258e8eb29a7a8f2b0c7e9e47a
Mayank Mohan <71745873+Mayank-032@users.noreply.github.com>
2022-08-28 09:23:53 +0530

    authentication (#3)

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ require('dotenv').config();
 var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var signinRoute = require("./controllers/signin.js");
 var signupRoute = require("./controllers/signup.js");
+var logoutRoute = require("./controllers/logout")
+var auth = require("./middleware/auth")
 
 var app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(cookieParser());
 
 // Database connection
 mongoose.connect(process.env.DB_CONNECTION_STRING);
@@ -22,8 +26,10 @@ database.once('connected', () => {
     console.log('Database Connected');
 })
 
-// app.use("/signin", signinRoute);
+app.use("/auth", auth);
+app.use("/signin", signinRoute);
 app.use("/signup", signupRoute);
+app.use("/logout", logoutRoute);
 
 app.listen(3000, function () {
   console.log("Working on 3000!!!");
